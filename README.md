317a282be9188d896b5c7108f317e5959f6fcf95
Xueheng Wan <32476223+wwanxh@users.noreply.github.com>
2020-10-29 16:59:03 -0700

    Upload

---
+++ README.md
@@ -0,0 +1,15 @@
+Template code for A04
+------------
+
+Author: Xueheng Wan [wanxueheng@email.arizona.edu](mailto:wanxueheng@email.arizona.edu)  
+Date: Oct 20, 2020
+
+**PLEASE UPDATE THIS README**
+
+
+Included files (**PLEASE ADD/UPDATE THIS LIST**):
+* scenes/ -- a folder with four input scenes to test
+* outputs/ -- a folder with output ppm images for each scene
+
+
+**PLEASE PROVIDE ANY ATTRIBUTION HERE**
---
+++ scenes/example1.js
@@ -0,0 +1,68 @@
+{
+    "eye": [ 0 , 0, 6 ],
+    "lookat": [0, 0, 3 ],
+    "up": [0,1,0],
+    "fov_angle": 99,
+    "width": 329,
+    "height": 329,
+    "MaxDepth": 4,
+    "SunLocation":[-900,900,0],
+    "spheres": [
+    {
+        "type": "sphere",
+        "center": [4,0,4],
+        "radius": 2,
+        "ambient": [0.09, 0.99,0.09],
+        "IsMirror":0
+    }
+    ],
+    "billboards": [
+	{
+    	"type":"billboards",
+        "UpperLeft": [ -8, 8 , -1], 
+        "LowerLeft": [-8 , -8, -1], 
+        "UpperRight": [8 , 8, -1],
+        "filename": "yael.jpg",
+        "IsMirror":0
+    }, 
+	{
+    	"type":"billboards",
+        "UpperLeft": [ -6, 3 , 0], 
+        "LowerLeft": [-6 , 1, 0], 
+        "UpperRight": [-4 , 3, 0],
+        "filename": "chessboard.jpg",
+        "IsMirror":0
+    }, 
+	{
+    	"type":"billboards",
+        "UpperLeft": [ 4, 3 , 0], 
+        "LowerLeft": [4 , 1, 0], 
+        "UpperRight": [6 , 3, 0],
+        "filename": "chessboard.jpg",
+        "IsMirror":0
+    }, 
+	{
+    	"type":"billboards",
+        "UpperLeft": [ 5, 5 , 7], 
+        "LowerLeft": [5 , -5, 7], 
+        "UpperRight": [-5,5,7],
+        "filename": "camera.jpg",
+        "IsMirror":0
+    },
+	{
+    	"type":"billboards",
+        "UpperLeft": [ 5, 5 , 7], 
+        "LowerLeft": [5 , -5, 7], 
+        "UpperRight": [-5,5,7],
+        "filename": "chessboard.jpg",
+        "IsMirror":1
+    },
+	{
+    	"type":"billboards",
+        "UpperLeft": [ -3, 3 , 0], 
+        "LowerLeft": [-3 , -3, 0], 
+        "UpperRight": [3, 3, 0],
+        "filename": "chessboard.jpg",
+        "IsMirror":1
+    }]
+}
